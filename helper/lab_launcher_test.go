2697db1d59e606bd3500f9cc56c133b6d9620b7b
Farul Wananda <farulahmad2002@gmail.com>
2023-04-27 14:02:11 +0700

    update: go unit test table(table test)

--- helper/lab_launcher_test.go
+++ helper/lab_launcher_test.go
@@ -9,6 +9,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Farul",
+			request:  "Farul",
+			expected: "Hello Farul",
+		},
+		{
+			name:     "Wananda",
+			request:  "Wananda",
+			expected: "Hello Wananda",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
 func TestSubTest(t *testing.T) {
 	t.Run("Farul", func(t *testing.T) {
 		result := HelloWorld("Farul")
